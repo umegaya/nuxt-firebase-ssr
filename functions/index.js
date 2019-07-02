077ab85f7f0cc043533f0680d0935cc11e359a68
Takehiro Iyatomi <iyatomi@gmail.com>
2019-07-02 15:58:18 +0900

    update nuxt and firebase modules except firease-cli

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,7 @@ const { Nuxt } = require('nuxt');
 const nuxt = new Nuxt({buildDir: 'ssr', dev: false});
 
 exports.ssr = functions.https.onRequest(nuxt.render);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
